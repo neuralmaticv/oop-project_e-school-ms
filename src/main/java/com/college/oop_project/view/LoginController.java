bb07fdf790473c2d8ba691abc10ffa15c5f700dd
Vladimir Mijic <vladocodes@gmail.com>
2022-01-09 20:05:43 +0100

    added option to show password

--- src/main/java/com/college/oop_project/view/LoginController.java
+++ src/main/java/com/college/oop_project/view/LoginController.java
@@ -2,9 +2,7 @@ package com.college.oop_project.view;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,7 +12,10 @@ public class LoginController implements Initializable {
     private TextField usernameInput;
 
     @FXML
-    private TextField userpassInput;
+    private PasswordField userpassInput;
+
+    @FXML
+    private TextField passwordText;
 
     @FXML
     private Label loginErrorMsg;
@@ -22,9 +23,24 @@ public class LoginController implements Initializable {
     @FXML
     private Button btnSubmit;
 
+    @FXML
+    private CheckBox showPassBtn;
+
     public void initialize(URL url, ResourceBundle resourceBundle) {
         btnSubmit.setOnAction(event -> {
             Controller.logInUser(event, loginErrorMsg, usernameInput, userpassInput);
         });
+    }
+
+    public void showPassword() {
+        if (showPassBtn.isSelected()) {
+            passwordText.setText(userpassInput.getText());
+            userpassInput.setVisible(false);
+            passwordText.setVisible(true);
+        } else {
+            userpassInput.setText(passwordText.getText());
+            userpassInput.setVisible(true);
+            passwordText.setVisible(false);
+        }
     }
 }
